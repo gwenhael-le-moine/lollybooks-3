38a17a240d448f3b22fbcd4a92f566cdb0534769
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2010-12-10 17:42:45 +0700

    cosmetics

--- static/js/jquery.slideshow.js
+++ static/js/jquery.slideshow.js
@@ -6,7 +6,8 @@
 function slideSwitch() {
     var $active = $('#slideshow IMG.active');
 
-    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+    if ( $active.length == 0 )
+        $active = $('#slideshow IMG:last');
 
     // use this to pull the images in the order they appear in the markup
     var $next =  $active.next().length ? $active.next() : $('#slideshow IMG:first');
